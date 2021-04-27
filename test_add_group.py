b8db991aaeb85ff82eaa7fa5ddd680daa51bec06
Elizaveta Solovey <elizavetasolovey@mail.ru>
2021-04-27 10:18:45 +0300

    Выполнен рефакторинг

--- test_add_group.py
+++ test_add_group.py
@@ -10,38 +10,51 @@ class TestAddGroup(unittest.TestCase):
         self.wd.implicitly_wait(30)
 
     def test_add_group(self):
-        dw = self.wd
-        # Open home page
-        dw.get("http://localhost/addressbook/")
-        # Login
-        dw.find_element_by_name("user").click()
-        dw.find_element_by_name("user").clear()
-        dw.find_element_by_name("user").send_keys("admin")
-        dw.find_element_by_name("pass").click()
-        dw.find_element_by_name("pass").clear()
-        dw.find_element_by_name("pass").send_keys("secret")
-        dw.find_element_by_xpath("//input[@value='Login']").click()
-        # Open groups page
-        dw.find_element_by_link_text("groups").click()
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd)
+        self.open_groups_page(wd)
+        self.create_group(wd)
+        self.return_to_groups_page(wd)
+        self.logout(wd)
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def return_to_groups_page(self, wd):
+        wd.find_element_by_link_text("group page").click()
+
+    def create_group(self, wd):
         # Init group creation
-        dw.find_element_by_name("new").click()
+        wd.find_element_by_name("new").click()
         # Fill group firm
-        dw.find_element_by_name("group_name").click()
-        dw.find_element_by_name("group_name").clear()
-        dw.find_element_by_name("group_name").send_keys("asd")
-        dw.find_element_by_name("group_header").click()
-        dw.find_element_by_name("group_header").clear()
-        dw.find_element_by_name("group_header").send_keys("asd")
-        dw.find_element_by_name("group_footer").click()
-        dw.find_element_by_name("group_footer").clear()
-        dw.find_element_by_name("group_footer").send_keys("ad")
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys("asd")
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys("asd")
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys("ad")
         # Submit group creation
-        dw.find_element_by_name("submit").click()
-        # Return to groups page
-        dw.find_element_by_link_text("group page").click()
-        # Logout
-        dw.find_element_by_link_text("Logout").click()
-    
+        wd.find_element_by_name("submit").click()
+
+    def open_groups_page(self, wd):
+        wd.find_element_by_link_text("groups").click()
+
+    def login(self, wd):
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/")
+
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
