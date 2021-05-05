45d5f009a0c3319c4f6cb8cd55727ecbb85c0cba
Elizaveta Solovey <elizavetasolovey@mail.ru>
2021-05-05 11:42:38 +0300

    Добавлен тест на редактирование первого контакта

--- fixture/contact.py
+++ fixture/contact.py
@@ -36,3 +36,29 @@ class ContactHelper:
     def return_to_home(self):
         wd = self.app.wd
         wd.find_element_by_link_text("home").click()
+
+    def edit_first_contact(self, contact):
+        wd = self.app.wd
+        # Edit
+        wd.find_element_by_xpath("//img[@alt='Edit']").click()
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(contact.address)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contact.email1)
+        wd.find_element_by_name("email2").click()
+        wd.find_element_by_name("email2").clear()
+        wd.find_element_by_name("email2").send_keys(contact.email2)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(contact.mobile)
+        # Update
+        wd.find_element_by_xpath("(//input[@name='update'])[2]").click()
+        self.return_to_home()
