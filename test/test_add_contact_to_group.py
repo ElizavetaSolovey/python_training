37d18567efb7b0123b82709181dfffd511febb08
Elizaveta Solovey <elizavetasolovey@mail.ru>
2021-05-11 15:09:46 +0300

    Добавлены предусловия в тесты удаление контакта и добавление контакта в дефолтную группу

--- test/test_add_contact_to_group.py
+++ test/test_add_contact_to_group.py
@@ -1,2 +1,6 @@
+from model.contact import Contact
+
 def test_add_contact_to_default_group(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="test", lastname="test", email1="test"))
     app.contact.test_add_contact_to_default_group()
