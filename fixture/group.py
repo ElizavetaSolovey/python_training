4f93228c2147a5492deb7b2e16c84c69485c36ce
Elizaveta Solovey <elizavetasolovey@mail.ru>
2021-05-11 11:28:01 +0300

    Обновлен тест на добавление групп с частичным дополнением Добавлено предусловие в тест modify_group

--- fixture/group.py
+++ fixture/group.py
@@ -7,22 +7,14 @@ class GroupHelper:
         wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
 
-    def create(self, group):
+    def create(self, new_group_data):
         wd = self.app.wd
         self.open_groups_page()
         # Init group creation
         wd.find_element_by_name("new").click()
-        # Fill group firm
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # Submit group creation
+        # Fill group form
+        self.fill_group_form(new_group_data)
+        # submit create
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
 
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -5,7 +5,6 @@ from model.group import Group
 
 def test_add_group(app):
     app.group.create(Group(name="new1", header="new1", footer="new1"))
-   # app.session.logout()
 
 
 def test_add_empty_group(app):
