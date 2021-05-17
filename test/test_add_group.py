3a0281b344bcf8b13a52119f53ad056f19eb0a1f
Elizaveta Solovey <elizavetasolovey@mail.ru>
2021-05-17 14:44:47 +0300

    Добавлен assert на добавление группы

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -4,7 +4,10 @@ from model.group import Group
 
 
 def test_add_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="new1", header="new1", footer="new1"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 
 def test_add_empty_group(app):
